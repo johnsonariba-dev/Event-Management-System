82d32425862ba502c1e7ad2b5efe18502d9b54f4
valerie988 <neh16valerie@gmail.com>
2025-09-03 11:44:00 +0100

    Events page

--- Frontend/src/types/images.ts
+++ Frontend/src/types/images.ts
@@ -6,4 +6,9 @@ import login from "../assets/images/login.png"
 export default{
     logo,
     login,
+import EventHero from "../assets/images/EventHero.png"
+
+export default{
+    logo,
+    EventHero
 }
