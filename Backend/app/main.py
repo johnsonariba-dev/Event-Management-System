5261e67ae5de7e0be81a4a36566c8d826e6ec12c
johnsonariba-dev <samueljohnson200816@gmail.com>
2025-10-02 20:16:26 +0100

    prepare for railway

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -94,4 +94,4 @@ def create_default_admin():
 # ---------------- Run Locally ----------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
